c1178f3b290142feb3c7748b1d70c4b4863cc5b8
Lucas Cordeiro <lucas.cordeiro@rarolabs.com.br>
2021-09-16 21:32:56 -0300

    Corrige porta

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(process.env.port, () => {
   console.log(`Example app listening at http://localhost:${process.env.port}`)
 })
