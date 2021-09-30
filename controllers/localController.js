1822dcce22dba131d4fc6afa9426e72f5d580019
Lucas Cordeiro <cordeirolucas42@hotmail.com>
2021-09-29 22:32:52 -0300

    Adiciona pesquisa por data

--- controllers/localController.js
+++ controllers/localController.js
@@ -1,2 +1,95 @@
 const pool = require('../db/connect');
+motoController = require('../controllers/motoController')
 
+const locaisFields = 'endereco, lotacao'
+const motosFields = 'placa, manutencao, alugada, aluguel_data_retirada, aluguel_data_entrega, aluguel_local_retirada, aluguel_local_entrega, reservada, reserva_data_retirada, reserva_data_entrega, reserva_local_retirada, reserva_local_entrega'
+
+exports.getAll = async (req, res) => {
+    const result = await pool.query('select * from locais')
+    res.send(result.rows)
+}
+
+exports.getAvaiableWhen = async (req, res) => {
+    let { cep, inicio, fim } = req.params
+    inicio = new Date(inicio)
+    fim = new Date(fim)
+
+    let query = `select ${motosFields},endereco from motos left join locais using (local_id) order by endereco`
+    let result = await pool.query(query)
+    let motos = await motoController.expandLocal(result.rows)
+
+    result = await pool.query(`select ${locaisFields} from locais`)
+    let locais = result.rows
+
+    locais =
+        locais.map(local => {
+            local.motos = motos.filter(moto => {
+                return (
+                    !moto.manutencao &&
+                    !estaraReservadaEm(moto, inicio, fim) &&
+                    !estaraAlugadaEm(moto, inicio, fim) &&
+                    local.endereco === motoEstaraEm(moto, inicio)
+                )
+            }).length
+
+            return local
+        })
+
+    res.send(locais)
+}
+
+const estaraAlugadaEm = (moto, inicio, fim) => {
+    const [
+        aluguel_data_entrega,
+        aluguel_data_retirada,
+        reserva_data_entrega,
+        reserva_data_retirada
+    ] = to_date(moto)
+
+    return (
+        moto.alugada &&
+        aluguel_data_retirada <= inicio && inicio <= aluguel_data_entrega ||
+        aluguel_data_retirada <= fim && fim <= aluguel_data_entrega
+    )
+}
+
+const estaraReservadaEm = (moto, inicio, fim) => {
+    const [
+        aluguel_data_entrega,
+        aluguel_data_retirada,
+        reserva_data_entrega,
+        reserva_data_retirada
+    ] = to_date(moto)
+
+    return (
+        moto.reservada &&
+        reserva_data_retirada <= inicio && inicio <= reserva_data_entrega ||
+        reserva_data_retirada <= fim && fim <= reserva_data_entrega
+    )
+}
+
+const motoEstaraEm = (moto, data) => {
+    const [
+        aluguel_data_entrega,
+        aluguel_data_retirada,
+        reserva_data_entrega,
+        reserva_data_retirada
+    ] = to_date(moto)
+
+    if (reserva_data_entrega && reserva_data_entrega <= data) {
+        return moto.reserva_local_entrega
+    } else if (aluguel_data_entrega && aluguel_data_entrega <= data) {
+        return moto.aluguel_local_entrega
+    } else {
+        return moto.endereco
+    }
+}
+
+const to_date = (moto) => {
+    return [
+        moto.aluguel_data_entrega,
+        moto.aluguel_data_retirada,
+        moto.reserva_data_entrega,
+        moto.reserva_data_retirada
+    ].map(d => d ? new Date(d) : null)
+}
--- routes/motos.js
+++ routes/motos.js
@@ -10,20 +10,9 @@ motosRoutes.get('/', motoController.getAllWithLocal)
 motosRoutes.get('/when/:date', motoController.getAllWhen)
 
 motosRoutes.get('/:id', async (req, res) => {
-const { id } = req.params
-console.log(`get /motos/:${id}`)
-const { rows } = await pool.query('SELECT * FROM motos WHERE id = $1', [id])
-res.send(rows[0])
+    const { id } = req.params
+    const { rows } = await pool.query('SELECT * FROM motos WHERE id = $1', [id])
+    res.send(rows[0])
 })
-  
-// Não tá funcionando ainda
-// motosRoutes.post('/motos', async (req, res) => {
-//   console.log("post /motos")
-//   const query = 'INSERT INTO motos(marca,modelo,preco,ano,cor,cambio) VALUES($1, $2, $3, $4, $5, $6)'
-//   console.log((req))
-//   const fields = Object.values(req.body.moto)
-//   await pool.query(query, fields)
-//   res.redirect("/motos")
-// })
 
 module.exports = motosRoutes
